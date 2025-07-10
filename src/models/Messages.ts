30312710f3a7fc4ad134775079f4e818928f9c51
Aarushi Daksh <aarushi312004@gmail.com>
2025-07-11 00:26:42 +0530

    payment

--- src/models/Messages.ts
+++ src/models/Messages.ts
@@ -1,22 +1,9 @@
-import mongoose, { Schema, models, model } from "mongoose";
+import mongoose, { Schema, model, models } from "mongoose";
 
-const MessageSchema = new Schema(
-  {
-    conversationId: {
-      type: Schema.Types.ObjectId,
-      ref: "Conversation",
-      required: true,
-    },
-    sender: {
-      type: String, 
-      required: true,
-    },
-    text: {
-      type: String,
-      required: true,
-    },
-  },
-  { timestamps: true }
-);
+const MessageSchema = new Schema({
+  conversationId: { type: Schema.Types.ObjectId, ref: "Conversation", required: true },
+  sender: { type: String, required: true },
+  text: { type: String, required: true },
+}, { timestamps: true });
 
-export default models.Message || model("Message", MessageSchema, "messages");
+export default models.Message || model("Message", MessageSchema);
